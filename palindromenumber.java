133f67823715d6bd00b36e1b476e83eb5de3f387
Gowthamichinta02 <85173082+Gowthamichinta02@users.noreply.github.com>
2023-02-02 15:45:49 +0530

    Create palindromenumber.java

---
+++ palindromenumber.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String s=String.valueOf(x);
+        String rev=new StringBuilder(s).reverse().toString();
+        if(s.equals(rev)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}

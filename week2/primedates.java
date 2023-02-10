929dcb76f77fe6a334a7a23a7028ee1b6c7be3b6
Gowthamichinta02 <85173082+Gowthamichinta02@users.noreply.github.com>
2023-02-10 11:34:17 +0530

    Create primedates.java

---
+++ week2/primedates.java
@@ -0,0 +1,28 @@
+ public static int findPrimeDates(int d1, int m1, int y1, int d2, int m2, int y2) {
+        storeMonth();
+    
+        int result = 0;
+    
+        while(true) {
+            int x = d1;
+            x = x * 100 + m1;
+            x = x * 10000 + y1;
+            if(x % 4 == 0 || x % 7 == 0) {
+                result = result + 1;
+            }
+            if(d1 == d2 && m1 == m2 && y1 == y2) {
+                break;
+            }
+            updateLeapYear(y1);
+            d1 = d1 + 1;
+            if(d1 > month[m1]) {
+                m1 = m1 + 1;
+                d1 = 1;
+                if(m1 > 12) {
+                    y1 =  y1 + 1;
+                    m1 =  1;
+                }
+            }
+        }
+        return result;
+    }

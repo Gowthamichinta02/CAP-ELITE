024807854b490bdf490a6dbdef4b6a06925df5a0
Gowthamichinta02 <85173082+Gowthamichinta02@users.noreply.github.com>
2023-02-10 15:40:03 +0530

    Create reverse-a-linked-list.java

---
+++ week3/reverse-a-linked-list.java
@@ -0,0 +1,15 @@
+ public static SinglyLinkedListNode reverse(SinglyLinkedListNode llist) {
+    // Write your code here
+        SinglyLinkedListNode next = null;
+        SinglyLinkedListNode current = llist;
+        SinglyLinkedListNode previous = null;
+        while(current!=null)
+        {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        llist= previous;
+        return llist;
+    }
